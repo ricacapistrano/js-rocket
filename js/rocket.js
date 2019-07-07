68233630d7f9339765646eaaf1d51f5f8f5177ac
ricalearns <52093463+ricalearns@users.noreply.github.com>
2019-07-07 10:22:42 +0800

    made our rocket cool with drawings

--- js/rocket.js
+++ js/rocket.js
@@ -17,8 +17,18 @@ var changeState = function (state){
 			document.getElementById('countdown').innerHTML = 
 			countdownNumber;
 		
+	if (countdownNumber > 4 && countdownNumber <= 7) {
+		//nervous
+		document.getElementById('nervous').className= 'nervous show';
+	} else {document.getElementById('nervous').className= 'nervous';}
 
-			if (countdownNumber<=0){
+	if (countdownNumber > 1 && countdownNumber <= 4) {
+		//can't wait
+		document.getElementById('cant-wait').className= 'cant-wait show';
+	}
+		else { document.getElementById('cant-wait').className= 'cant-wait';};
+
+	if (countdownNumber<=0){
 				changeState(3);
 			};
 	
